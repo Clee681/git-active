fb9a8811209ab39a638b43b2b8a5d24931829cee
Clee681 <christopher.lee681@gmail.com>
2013-10-15 20:54:58 -0400

    Minor changes during demo to team

--- api_caller.rb
+++ api_caller.rb
@@ -15,25 +15,24 @@ class ApiCaller
 
       # what gets added dependent on time created
       events_to_add = api_results.collect do |element|
-        if element.attrs[:created_at] > self.time && element.attrs[:type] == "PushEvent"
+        if element.attrs[:created_at] > self.time #&& element.attrs[:type] == "PushEvent"
           Event.new.tap do |event|
             user_match = User.all.select { |u| u.name == element.attrs[:actor].attrs[:login] }
 
             if user_match.empty?
-              event.name  = User.new(element.attrs[:actor].attrs[:login])
+              event.name = User.new(element.attrs[:actor].attrs[:login], element.attrs[:actor].attrs[:gravatar_id])
             else
               event.name = user_match.first
             end
             
-            event.repo  = element.attrs[:repo].attrs[:name]
-            event.type  = element.attrs[:type]
-            event.time  = element.attrs[:created_at]
+            event.repo   = element.attrs[:repo].attrs[:name]
+            event.type   = element.attrs[:type]
+            event.time   = element.attrs[:created_at]
             puts "#{element.attrs[:actor].attrs[:login]} at #{element.attrs[:created_at]}"
           end
         end
       end
 
-      
       self.time = Time.now.utc
       binding.pry
       sleep 120
--- user.rb
+++ user.rb
@@ -1,11 +1,19 @@
 class User
-  attr_accessor :name
+  attr_accessor :name, :avatar
   
   @@users = []
 
-  def initialize(name)
+  # @@db = SQLite3::Database.new 'users.db'
+  # @@db.execute("CREATE TABLE IF NOT EXISTS users
+  #               (id INTEGER PRIMARY KEY AUTOINCREMENT,
+  #               name TEXT,
+  #               avatar TEXT);"
+  #             )
+
+  def initialize(name, avatar)
     @@users << self
-    self.name = name
+    self.name   = name
+    self.avatar = avatar
   end
 
   def self.all
